e50e2b03aafa1e09c711d378277741793dcdd959
Hugo Duchene <duchenehugo10@gmail.com>
2020-06-01 11:48:43 +0100

    fonctionalité score

--- score.py
+++ score.py
@@ -6,29 +6,45 @@ from settings import *
 
 class point:
 
-    def __init__(self):
+    def __init__(self, map_class):
         self.map = Map()
-        self.map = self.map
         self.position = position()
         self.pos_computer  = (0, 0)
         self.pos_hero = (0, 0)
         self.point = 0
         self.nbs_objects = 0
+        self.list_objects = self.map.put_objetcs(map_class)
+        
     
-    def count_points(self, map_class):
+    def count_points(self, map_class, new_map):
         for m in self.position.research_pos(map_class, hero):
-            self.pos_hero = m 
-        for x, y in self.position.research_pos(self.map.list_labyrinth, objects):
-            self.pos_computer = (x, y)
-            
-            if self.pos_hero == (100, 0) and self.point < 1:
-                self.point = 1
-            if self.pos_hero == (450, 150) and self.point < 2:
-                self.point = 2
-            if self.pos_hero == (400, 500) and self.point < 3:
-                self.point = 3
+            self.pos_hero = m
+            (x_hero, y_hero) = self.pos_hero
+            x_hero = int(x_hero/50)
+            y_hero = int(y_hero/50)
+        
+        for pos_objects in self.list_objects:
+            if pos_objects == self.pos_hero:
+                if new_map[y_hero][x_hero] == "u":
+                    self.point += 1
+                    new_map[y_hero][x_hero] = "0"
+                elif new_map[y_hero][x_hero] == "0":
+                    pass
         return self.point
                 
+    
+    def generate_new_map(self):
+        for loop in self.list_objects:
+            (x, y) = loop
+            x = int(x/50)
+            y = int(y/50)
+            new_map = self.map.list_labyrinth 
+            new_map[y][x] = "u"
+        return new_map
+        
+        
+        
+        
         
     
 
