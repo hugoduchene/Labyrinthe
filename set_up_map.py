3ac9a52430b4a4d85953ff2ad950995d326fa06c
Hugo Duchene <duchenehugo10@gmail.com>
2020-06-01 09:52:25 +0100

    mattre les objets de manière aléatoire

--- set_up_map.py
+++ set_up_map.py
@@ -1,7 +1,10 @@
 from settings import * 
-
+from random import randint
+from position import position
 class Map:
     def __init__(self):
+        self.list_objects = []
+        self.position = position()
         self.list_labyrinth = self.load_map()
         self.list_labyrinth = [list(s) for s in self.list_labyrinth]
     
@@ -18,6 +21,30 @@ class Map:
     def list_map(self):
         return self.list_labyrinth
 
+    def put_objetcs(self, map_class):
+        place_object = self.position.research_pos(map_class, path_free)
+
+        for loop in range(3):
+            length_list = len(place_object)
+            nbs_random = randint(0, length_list)
+            self.list_objects.append(place_object[nbs_random])
+            (x, y) = self.list_objects[loop]
+            x = int(x/50)
+            y = int(y/50)
+            map_class[y][x] = objects
+            
+        
+        return self.list_objects 
+
+        
+
+                
+
+        
+        
+        
+        
+        
 
 
 
